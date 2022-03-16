c71ad2a5e58f9603fd0a569d83723dbb23650167
Douglas <dodosobreira@gmail.com>
2022-03-16 19:32:44 -0300

    add time counter

--- src/App.java
+++ src/App.java
@@ -1,21 +1,35 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 class App {
   public static void main(String[] args) {
     App app = new App();
-    List <Integer> list = app.generateList(32);
-    list = app.mergeSort(list);
-    System.out.println(list);
+    List <Integer> list1 = app.generateList(32); // caso 1
+    List <Integer> list2 = app.generateList(1024); // caso 2
+    List <Integer> list3 = app.generateList(1048576); // caso
 
+    long time1 = app.countTime(() -> app.mergeSort(list1));
+    long time2 = app.countTime(() -> app.mergeSort(list2));
+    long time3 = app.countTime(() -> app.mergeSort(list3));
+
+    System.out.println("Caso 1: " + time1 + " ms");
+    System.out.println("Caso 2: " + time2 + " ms");
+    System.out.println("Caso 3: " + time3 + " ms");
+
+  }
+
+  // count time function in milliseconds
+  public long countTime(Runnable r) {
+    long start = System.currentTimeMillis();
+    r.run();
+    long end = System.currentTimeMillis();
+    return end - start;
   }
 
   public List<Integer> generateList(int size) {
     List<Integer> list = new ArrayList<>();
     for (int i = 0; i < size; i++) {
-      list.add((int) (Math.random() * 100));
+      list.add((int) (Math.random() * 1024));
     }
     return list;
   }
